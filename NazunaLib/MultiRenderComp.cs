78cb7b6f71afbc06bc5fc939c7f9acc3d9284ab9
narei_6622 <MikuVick7139@gmail.com>
2025-09-14 06:55:23 +0900

    20250914

--- NazunaLib/MultiRenderComp.cs
+++ NazunaLib/MultiRenderComp.cs
@@ -188,9 +188,9 @@ namespace NareisLib
             }
         }
 
-        public override void PostDeSpawn(Map map)
+        public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
         {
-            base.PostDeSpawn(map);
+            base.PostDeSpawn(map, mode);
             foreach (TextureLevels t in cachedAllOriginalDefForGraphicDataList)
             {
                 if (t.actionManager.def != null)
